9d655cad3104e3c0ddbd345bda51dd286305a49f
alexmansourd <me@alexmansour.ch>
2025-05-08 19:19:18 +0200

    hide api token

--- src/main/java/ch/alexmansour/metta/Main.java
+++ src/main/java/ch/alexmansour/metta/Main.java
@@ -6,11 +6,9 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws IOException, TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(new TelegramBot());
     }
--- src/main/java/ch/alexmansour/metta/TelegramBot.java
+++ src/main/java/ch/alexmansour/metta/TelegramBot.java
@@ -3,26 +3,12 @@ package ch.alexmansour.metta;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-
 public class TelegramBot extends TelegramLongPollingBot {
 
     private final static String BOT_NAME = "Metta-Bot";
 
-    public TelegramBot() throws IOException {
-        super(getToken());
-    }
-
-    private static String getToken() throws IOException {
-        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-        String appConfigPath = rootPath + "app.properties";
-
-        Properties appProps = new Properties();
-        appProps.load(new FileInputStream(appConfigPath));
-        return appProps.getProperty("api.bot.token");
+    public TelegramBot() {
+        super(ApiToken.API_TOKEN);
     }
 
     @Override
