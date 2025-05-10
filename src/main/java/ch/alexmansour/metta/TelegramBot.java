b7080d219580ea310fbd978f59c554611d7cff64
alexmansourd <me@alexmansour.ch>
2025-05-10 09:59:00 +0200

    implement echo test function

--- src/main/java/ch/alexmansour/metta/TelegramBot.java
+++ src/main/java/ch/alexmansour/metta/TelegramBot.java
@@ -1,7 +1,10 @@
 package ch.alexmansour.metta;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TelegramBot extends TelegramLongPollingBot {
 
@@ -13,11 +16,23 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        Message msg = update.getMessage();
+        sendText(msg.getFrom().getId(), msg.getText());
     }
 
     @Override
     public String getBotUsername() {
         return BOT_NAME;
+    }
+
+    public void sendText(Long who, String what) {
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
     }
 }
